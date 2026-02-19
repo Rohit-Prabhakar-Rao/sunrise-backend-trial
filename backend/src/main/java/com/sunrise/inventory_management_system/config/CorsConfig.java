e7eb0e4e4054f66c32e89edb10023622f9978c38
Rohit-Prabhakar-Rao <rohitrao1411@gmail.com>
2026-02-20 02:06:36 +0530

    cors changes

--- backend/src/main/java/com/sunrise/inventory_management_system/config/CorsConfig.java
+++ backend/src/main/java/com/sunrise/inventory_management_system/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.sunrise.inventory_management_system.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -7,15 +8,19 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Configuration
 public class CorsConfig {
+    @Value("${ALLOWED_ORIGIN}")
+    private String allowedOrigin;
 
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://localhost:8081", "https://sunrise-fe.vercel.app/"));
+//        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://localhost:8081", "https://sunrise-fe.vercel.app/"));
+        configuration.setAllowedOrigins(Collections.singletonList(allowedOrigin));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept", "Origin"));
         configuration.setAllowCredentials(true);
